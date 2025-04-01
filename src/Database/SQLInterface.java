270993d6cbe9002d6b7569fdf4d20d6827062653
suspecttaco <eladielgomez@gmail.com>
2025-04-01 14:46:54 -0700

    first sql attemp

---
+++ src/Database/SQLInterface.java
@@ -0,0 +1,9 @@
+package Database;
+
+import java.sql.Connection; //SQL library
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class SQLInterface {
+    //TODO: MySQL connection
+}
