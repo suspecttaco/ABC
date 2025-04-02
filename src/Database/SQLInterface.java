92eb3a04b9f85d1c966760554da62cb477a66227
suspecttaco <eladielgomez@gmail.com>
2025-04-01 18:00:59 -0700

    added sql code template

--- src/Database/SQLInterface.java
+++ src/Database/SQLInterface.java
@@ -6,4 +6,22 @@ import java.sql.SQLException;
 
 public class SQLInterface {
     //TODO: MySQL connection
+    public static void main(String[] args) {
+        String connectionUrl =
+                "jdbc:sqlserver://yourserver.database.windows.net:1433;"
+                        + "database=AdventureWorks;"
+                        + "user=yourusername@yourserver;"
+                        + "password=<password>;"
+                        + "encrypt=true;"
+                        + "trustServerCertificate=false;"
+                        + "loginTimeout=30;";
+
+        try (Connection connection = DriverManager.getConnection(connectionUrl);) {
+            // Code here.
+        }
+        // Handle any errors that may have occurred.
+        catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 }
