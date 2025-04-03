1cbd897437e3eb5626c6cd745a90abd7ac70df16
suspecttaco <eladielgomez@gmail.com>
2025-04-02 22:11:06 -0700

    update

--- src/Database/SQLInterface.java
+++ src/Database/SQLInterface.java
@@ -1,8 +1,8 @@
 package Database;
 
-import java.sql.Connection; //SQL library
-import java.sql.DriverManager;
-import java.sql.SQLException;
+// import java.sql.Connection; //SQL library
+// import java.sql.DriverManager;
+// import java.sql.SQLException;
 
 public class SQLInterface {
     //TODO: MySQL connection
