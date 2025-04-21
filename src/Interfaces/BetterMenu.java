b40004d81a8a9725b3e4742bf2c41221cada27ff
suspecttaco <eladielgomez@gmail.com>
2025-04-20 17:37:52 -0700

    Nueva interfaz en progreso

---
+++ src/Interfaces/BetterMenu.java
@@ -0,0 +1,15 @@
+package Interfaces;
+
+import javax.swing.*;
+
+public class BetterMenu {
+    private JPanel panel;
+    private JTabbedPane tabbedPane1;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JButton button1;
+    private JButton button2;
+    private JButton button3;
+    private JButton button4;
+    private JTextArea textArea1;
+}
